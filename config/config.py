058cb935c30e23354a949069206780a3d70cd200
Shahriar Alom Masud <145286840+Masud744@users.noreply.github.com>
2026-02-15 04:06:13 +0600

    Change InfluxDB token in config.py
    Updated InfluxDB token while keeping other configurations unchanged.

--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 INFLUX_URL = "https://us-east-1-1.aws.cloud2.influxdata.com"
-INFLUX_TOKEN = "xofuKrVX09Gr61IUkx6uG6K2zuuFA1Qc-n9sSXEizdCVQtt9Wj7LifFUp2GHyYx9sCTFPEr_WwiTKKyHHkopRw=="
+INFLUX_TOKEN = "tYGgtBrpwtI4-qJEZQmYc4ZaxsW60o9LCdgwm3QSgeAJM-rIz2zhsHZiQGjFwka_T3qOIClv9hPFyJ9-aSlRXQ=="
 INFLUX_ORG = "64d08bf112f03d85"
 INFLUX_BUCKET = "kaliakair_weather_2026"
 
 LOCATION = "kaliakair"
+
